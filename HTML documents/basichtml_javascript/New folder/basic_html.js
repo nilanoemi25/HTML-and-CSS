5b9e12220e88d538e3236f11b699e1f488d0b405
nilanoemi25 <nila.noemi25@gmail.com>
2021-10-11 16:28:03 +0100

    New html practices

---
+++ HTML documents/basichtml_javascript/New folder/basic_html.js
@@ -0,0 +1,1 @@
+alert("This is assignment 11");
---
+++ HTML documents/basichtml_javascript/New folder/function.js
@@ -0,0 +1,6 @@
+ function My_First_Function() {
+       var String = "Kiss me, I'm Irish!";
+       var restult = String.fontcolor("green");
+	   document.getElementById("Irish").innerHTML = 
+	   result;
+	   } 
